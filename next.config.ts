8c2a8f33c1821978ee5fb59b3414c187eb427459
Thang Vu <thangvq95@gmail.com>
2026-04-10 10:44:36 +0700

    Replace hideSourceMaps with sourcemaps option
    Update Sentry Next.js config in next.config.ts: remove the deprecated hideSourceMaps: true and use the new sourcemaps configuration with deleteSourcemapsAfterUpload: true. This ensures source maps are uploaded to Sentry then removed from build artifacts to avoid exposing source while keeping bundles smaller.

--- next.config.ts
+++ next.config.ts
@@ -73,7 +73,9 @@ export default withSentryConfig(nextConfig, {
   tunnelRoute: "/monitoring",
 
   // Hides source maps from generated client bundles
-  hideSourceMaps: true,
+  sourcemaps: {
+    deleteSourcemapsAfterUpload: true,
+  },
 
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
